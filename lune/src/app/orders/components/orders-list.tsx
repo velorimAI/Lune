586bb91e502ee7722d5f5a59ebeef09b482c189f
velorimAi <velorimai@gamil.com>
2025-05-11 10:15:26 +0300

    modal add order

--- lune/src/app/orders/components/orders-list.tsx
+++ lune/src/app/orders/components/orders-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC } from "react";
-import { Info } from "lucide-react";
+import { CircleArrowLeft } from "lucide-react";
 
 interface Order {
   fullName: string;
@@ -18,32 +18,35 @@ interface OrdersListProps {
 
 export const OrdersList: FC<OrdersListProps> = ({ data }) => {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 ">
       {data.map((order: Order, index: any) => (
         <div
           key={index}
-          className="grid grid-cols-7 bg-white shadow-sm px-4 py-3 text-xs w-full border border-gray-300 rounded-lg items-center gap-2 text-center"
+          className="grid grid-cols-7 bg-gray-50 shadow-sm px-4 py-3 text-xs w-full border border-gray-300 rounded-lg items-center gap-2 text-center"
         >
           <div>نام و نام خانوادگی: {order.fullName}</div>
           <div>شماره تلفن: {order.phone}</div>
           <div>تاریخ سفارش: {order.orderDate}</div>
           <div>تاریخ تحویل: {order.deliveryDate}</div>
           <div>شماره پذیرش: {order.orderNumber}</div>
-          <div>
-            وضعیت:{" "}
+          <div className="space-x-1">
+            <span>وضعیت:</span>
             <span
               className={
                 order.status === "تسویه نشده"
-                  ? "text-red-500"
-                  : "text-green-600"
+                  ? "text-red-500 font-bold"
+                  : "text-green-600 font-bold"
               }
             >
               {order.status}
             </span>
           </div>
           <div className="flex justify-center">
-            <Info className="w-4 h-4 text-blue-500" />
+            <CircleArrowLeft className="w-6 h-5" />
           </div>
+          {/* <div className="flex justify-center">
+            <Info className="w-4 h-4 text-blue-500" />
+          </div> */}
         </div>
       ))}
     </div>
--- lune/src/app/orders/page.tsx
+++ lune/src/app/orders/page.tsx
@@ -1,112 +1,235 @@
-"use client";
+// "use client";
 
-import { FC, useState } from "react";
-import { OrdersList } from "./components/orders-list";
-import { mockOrders } from "@/mock/ordersListData";
-import { useStaticSearchDevices } from "./hooks/use-static-search-devices";
-import { SearchBox } from "../components/table/search-box";
-import { Card } from "../components/card";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { CirclePlus } from "lucide-react";
-import { Modal } from "../components/modal";
-import { Form } from "../components/custom-form/form";
-import { Input } from "../components/custom-form/input";
-import { Select } from "../components/custom-form/select-box";
+// import { FC, useState } from "react";
+// import { OrdersList } from "./components/orders-list";
+// import { mockOrders } from "@/mock/ordersListData";
+// import { useStaticSearchDevices } from "./hooks/use-static-search-devices";
+// import { SearchBox } from "../components/table/search-box";
+// import { Card } from "../components/card";
+// import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+// import { CirclePlus, Trash2 } from "lucide-react";
+// import { Modal } from "../components/modal";
+// import { Form } from "../components/custom-form/form";
+// import { Input } from "../components/custom-form/input";
+// import { Select } from "../components/custom-form/select-box";
 
+// const sortOptions = [
+//   { value: "default", label: "مرتب‌سازی پیش‌فرض" },
+//   { value: "name", label: "مرتب‌سازی بر اساس نام" },
+//   { value: "receptionDate", label: "مرتب‌سازی بر اساس تاریخ پذیرش" },
+// ];
 
-const Orders: FC = () => {
-  const [searchText, setSearchText] = useState<string>("");
-  const [open, setOpen] = useState(false);
-  const [orders, setOrders] = useState(mockOrders);
+// const Orders: FC = () => {
+//   const [searchText, setSearchText] = useState<string>("");
+//   const [open, setOpen] = useState(false);
+//   const [orders, setOrders] = useState(mockOrders);
+//   const [sortBy, setSortBy] = useState<string>("default");
 
-  const filteredDataList = useStaticSearchDevices(orders || [], searchText);
+//   const [parts, setParts] = useState([{ name: "", quantity: 1 }]);
 
-  const handleSearch = (value?: string) => {
-    setSearchText(value || "");
-  };
+//   const filteredDataList = useStaticSearchDevices(orders || [], searchText);
 
-  const handleOpen = (e?: React.MouseEvent) => {
-    e?.stopPropagation?.();
-    setOpen(true);
-  };
+//   const handleSearch = (value?: string) => {
+//     setSearchText(value || "");
+//   };
 
-  const handleSubmit = (data: any) => {
-    const newOrder = {
-      id: Date.now(), // یا UUID
-      ...data,
-    };
-    setOrders([newOrder, ...orders]);
-    setOpen(false);
-  };
+//   const sortedOrders = [...filteredDataList].sort((a, b) => {
+//     if (sortBy === "name") {
+//       return a.name.localeCompare(b.name);
+//     } else if (sortBy === "receptionDate") {
+//       return (
+//         new Date(b.receptionDate).getTime() -
+//         new Date(a.receptionDate).getTime()
+//       );
+//     }
+//     return 0;
+//   });
 
-  return (
-    <>
-      <Card title="سفارش ها">
-        <div className="w-[400px]">
-          <SearchBox onSearch={handleSearch} />
-        </div>
-        <ScrollArea className="w-full flex flex-col justify-start items-center pr-3 h-[73vh] 4xl:h-[80vh]">
-          <div dir="rtl">
-            <OrdersList data={filteredDataList || []} />
-          </div>
-          <div className="fixed left-10 bottom-[55px]">
-            <CirclePlus className="w-[30px] h-[30px] cursor-pointer" onClick={handleOpen} />
-          </div>
-          <ScrollBar />
-        </ScrollArea>
-      </Card>
+//   const handleOpen = (e?: React.MouseEvent) => {
+//     e?.stopPropagation?.();
+//     setOpen(true);
+//   };
 
-      <Modal
-        open={open}
-        title="افزودن سفارش جدید"
-        onCancel={() => setOpen(false)}
-        onConfirm={undefined}
-      >
-        <Form onSubmit={handleSubmit} submitText="ثبت" cancelText="انصراف" cancelHide submitHide>
-          <Input
-            label="نام کامل"
-            name="fullName"
-            placeholder="مثلاً: مبین خراسانی"
-            className="w-full"
-          />
-          <Input
-            label="شماره تماس"
-            name="phone"
-            placeholder="مثلاً: 09960028362"
-            className="w-full"
-          />
-          <Input
-            label="تاریخ سفارش"
-            name="orderDate"
-            placeholder="مثلاً: 1404/2/5"
-            className="w-full"
-          />
-          <Input
-            label="تاریخ تحویل"
-            name="deliveryDate"
-            placeholder="مثلاً: 1404/2/10"
-            className="w-full"
-          />
-          <Input
-            label="شماره سفارش"
-            name="orderNumber"
-            placeholder="مثلاً: 923243346"
-            className="w-full"
-          />
-          <Select
-            label="وضعیت سفارش"
-            name="status"
-            value="تسویه نشده"
-            options={[
-              { label: "تسویه نشده", value: "تسویه نشده" },
-              { label: "تسویه شده", value: "تسویه شده" },
-              { label: "در حال آماده‌سازی", value: "در حال آماده‌سازی" },
-            ]}
-          />
-        </Form>
-      </Modal>
-    </>
-  );
-};
+//   const handlePartChange = (
+//     index: number,
+//     field: string,
+//     value: string | number
+//   ) => {
+//     const updated = [...parts];
+//     updated[index][field] = value;
+//     setParts(updated);
+//   };
 
-export default Orders;
+//   const addPart = () => {
+//     setParts([...parts, { name: "", quantity: 1 }]);
+//   };
+
+//   const removePart = (index: number) => {
+//     const updated = [...parts];
+//     updated.splice(index, 1);
+//     setParts(updated);
+//   };
+
+//   const handleSubmit = (data: any) => {
+//     const newOrder = {
+//       id: Date.now(), // یا UUID
+//       ...data,
+//     };
+//     setOrders([newOrder, ...orders]);
+//     setOpen(false);
+//   };
+
+//   return (
+//     <>
+//       <Card title="سفارش ها">
+//         <div className="flex items-center gap-4 w-full max-w-[600px]">
+//           <div className="w-[300px]">
+//             <SearchBox onSearch={handleSearch} />
+//           </div>
+//           <Select
+//             value={sortBy}
+//             onChange={(selected) => setSortBy(selected?.value || "default")}
+//             options={sortOptions}
+//           />
+//           {/* <option value="default">مرتب‌سازی پیش‌فرض</option>
+//             <option value="name">مرتب‌سازی بر اساس نام</option>
+//             <option value="receptionDate">مرتب‌سازی بر اساس تاریخ پذیرش</option> */}
+//         </div>
+//         <ScrollArea className="w-full flex flex-col justify-start items-center pr-3 h-[73vh] 4xl:h-[80vh]">
+//           <div dir="rtl">
+//             <OrdersList data={sortedOrders || []} />
+//           </div>
+//           <div className="fixed left-10 bottom-[55px]">
+//             <CirclePlus
+//               className="w-[30px] h-[30px] cursor-pointer"
+//               onClick={handleOpen}
+//             />
+//           </div>
+//           <ScrollBar />
+//         </ScrollArea>
+//       </Card>
+
+//       <Modal
+//         open={open}
+//         title="افزودن سفارش جدید"
+//         onCancel={() => setOpen(false)}
+//         onConfirm={undefined}
+//         cancelText="لفو"
+//         confirmText="اضافه"
+//       >
+//         <Form
+//           onSubmit={handleSubmit}
+//           submitText="ثبت"
+//           cancelText="انصراف"
+//           cancelHide
+//           submitHide
+//         >
+//           <div className="flex gap-6 w-full">
+//             <div className="w-full">
+//               <Input
+//                 label="نام"
+//                 name="name"
+//                 placeholder="نام را وارد کنید :"
+//                 className="w-full"
+//                 required
+//               />
+//               <Input
+//                 label="نام خانوادگی"
+//                 name="lastname"
+//                 placeholder="نام خانوادگی را وارد کنید :"
+//                 className="w-full"
+//                 required
+//               />
+//               <Input
+//                 label="شماره تماس"
+//                 name="phone"
+//                 placeholder="******0912"
+//                 className="w-full"
+//                 required
+//               />
+//               <Input
+//                 label="شماره پذیرش:"
+//                 name="receptionNumber"
+//                 placeholder="******"
+//                 className="w-full"
+//                 required
+//               />
+//               <Input
+//                 label="تاریخ پذیرش:"
+//                 name="receptionDate"
+//                 placeholder="1404/01/01"
+//                 className="w-full"
+//                 required
+//               />
+//               <Input
+//                 label="تاریخ سفارش"
+//                 name="orderDate"
+//                 placeholder="مثلاً: 1404/2/5"
+//                 className="w-full"
+//               />
+//               <Input
+//                 label="شماره سفارش"
+//                 name="orderNumber"
+//                 placeholder="********"
+//                 className="w-full"
+//               />
+//             </div>
+
+//             {/* بخش قطعات */}
+//             <div>
+//               <h4 className="text-sm font-bol mb-2 text-right">قطعات</h4>
+//               {parts.map((part, index) => (
+//                 <div
+//                   key={index}
+//                   className="grid grid-cols-12 gap-2 items-end mb-2"
+//                 >
+//                   <div className="col-span-6">
+//                     <Input
+//                       label="نام قطعه"
+//                       value={part.name}
+//                       onChange={(e) =>
+//                         handlePartChange(index, "name", e.target.value)
+//                       }
+//                     />
+//                   </div>
+//                   <div className="col-span-4">
+//                     <Input
+//                       label="تعداد"
+//                       type="number"
+//                       value={String(part.quantity)}
+//                       onChange={(e) =>
+//                         handlePartChange(
+//                           index,
+//                           "quantity",
+//                           Number(e.target.value)
+//                         )
+//                       }
+//                     />
+//                   </div>
+//                   <div className="col-span-2">
+//                     <button
+//                       type="button"
+//                       onClick={() => removePart(index)}
+//                       className="p-2 text-red-500 hover:text-red-700"
+//                     >
+//                       <Trash2 className="w-4 h-4" />
+//                     </button>
+//                   </div>
+//                 </div>
+//               ))}
+//               <button
+//                 type="button"
+//                 onClick={addPart}
+//                 className="text-blue-600 text-xs hover:underline"
+//               >
+//                 + افزودن قطعه جدید
+//               </button>
+//             </div>
+//           </div>
+//         </Form>
+//       </Modal>
+//     </>
+//   );
+// };
+
+// export default Orders;
